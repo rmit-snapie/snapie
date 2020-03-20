b255159a18c0f79e9dc1d8281921c5d091adce2c
trungduong0103 <trungduong0103@gmail.com>
2020-03-20 16:12:44 +0700

    update .gitignore file to exclude .idea folder

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = require('express')('cors');
+const cors = require('cors');
+app.use(cors());
+
+const {getImageLabels} = require('./vision-functions/index');
+
+exports.getImageLabels = functions.region('asia-northeast1').https.onRequest(getImageLabels);

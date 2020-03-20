f84ba86a4fec569e278599d3cea69b47212814df
s3687636 <nguyenducminhtam83vn2003@gmail.com>
2020-03-20 14:54:26 +0700

    react native app with speech to text: english and vietnamese

---
+++ VoiceNative/components/VoiceNative.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, AppRegistry } from "react-native";
+import Voice from "react-native-voice";
+export default class VoiceNative extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recognized: "",
+      started: "",
+      results: []
+    };
+
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
+    onStop = this.onStop.bind(this);
+  }
+  onSpeechStart(e) {
+    this.setState({
+      started: "√"
+    });
+  }
+  onSpeechRecognized(e) {
+    this.setState({
+      recognized: "√"
+    });
+  }
+  onSpeechResults(e) {
+    this.setState({
+      results: e.value
+    });
+  }
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+  async _startRecognition(e) {
+    this.setState({
+      recognized: "",
+      started: "",
+      results: []
+    });
+    try {
+      console.log("start recording....");
+      await Voice.start("VI");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  async stopRecording() {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  onStop() {
+    console.log("stop recording!");
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+  render() {
+    console.log("state: ", this.state);
+    return (
+      <View>
+        <Text style={styles.transcript}>Transcript</Text>
+        {this.state.results.map((result, index) => (
+          <Text key={index} style={styles.transcript}>
+            {result}
+          </Text>
+        ))}
+        <Button
+          style={styles.transcript}
+          onPress={this._startRecognition.bind(this)}
+          title="Start"
+        ></Button>
+        <Button
+          style={styles.transcript}
+          onPress={this.onStop.bind(this)}
+          title="Stop"
+        ></Button>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  transcript: {
+    textAlign: "center",
+    color: "#B0171F",
+    marginBottom: 1,
+    top: "400%"
+  }
+});
+AppRegistry.registerComponent("VoiceNative", () => VoiceNative);
--- steps.md
+++ steps.md
@@ -14,3 +14,20 @@
   const mapStateToProps = state => ({books: state.bookReducer.books});
   const mapDispatchToProps = {dispatchAddBook: book => addBook(book)};
   export default connect(mapStateToProps, mapDispatchToProps)(Books)
+
+* [] react native with voice
+
+- set up
+  create-react-native-app VoiceNative
+  cd VoiceNative / npm run eject
+- dependencies:
+  npm i react-native-voice --save
+  react-native link react-native-voice
+- for ios: pod install
+  for android: local.confg
+- permission:
+  ios: ios/VoiceNative/Info.plist
+- components:
+  VoiceNative
+  App.js
+- Voice.start("VI") for vietnamese, us for english...

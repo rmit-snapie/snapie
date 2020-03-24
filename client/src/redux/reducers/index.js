ecc2220da88d10279ffe18864aefd497e5fcd23b
s3687636 <nguyenducminhtam83vn2003@gmail.com>
2020-03-24 20:43:50 +0700

    client/add basic testing code

--- client/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import userReducer from './UserReducer';
-
-const rootReducer = combineReducers({
-  userReducer: userReducer,
-});
-
-export default rootReducer;
--- client/src/redux/types.js
+++
@@ -1,2 +0,0 @@
-export const LOG_IN = 'LOG_IN';
-export const LOGGING_IN = 'LOGGING_IN';

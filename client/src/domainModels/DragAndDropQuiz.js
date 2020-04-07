83ca7276cb08403571f352c213aedacab5c81fe9
s3687636 <nguyenducminhtam83vn2003@gmail.com>
2020-04-07 17:19:12 +0700

    first try drag&drop

--- client/src/domainModels/DragAndDropQuiz.js
+++ client/src/domainModels/DragAndDropQuiz.js
@@ -1,21 +1,67 @@
 import React, {Component} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {Animated, View, StyleSheet, PanResponder, Text} from 'react-native';
 // import {goToFirstScreenInStack} from '../helpers/NavigateHelper';
 import PropTypes from 'prop-types';
-import {PanResponder, Animated} from 'react-native';
 
 class DragAndDropQuiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // this.onGestureEvent = Animated.event([
+    //   {
+    //     nativeEvent: {
+    //       translationX: this.dragX,
+    //       translationY: this.dragY,
+    //       state: this.gestureState,
+    //     },
+    //   },
+    // ]);
+  }
   static propTypes = {
     quizData: PropTypes.object.isRequired,
   };
+  pan = new Animated.ValueXY();
+  panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      this.pan.setOffset({
+        x: this.pan.x._value,
+        y: this.pan.y._value,
+      });
+    },
+    onPanResponderMove: Animated.event([
+      null,
+      {dx: this.pan.x, dy: this.pan.y},
+    ]),
+    onPanResponderRelease: () => {
+      console.log('release at: ', this.pan);
+      // alert('You dropped it at:' + this.pan.x._value + ' ' + this.pan.y._value);
+      this.pan.flattenOffset();
+      if (this.pan.y._value < 0) {
+        console.log('y < 0 : ', this.pan, 'moving back to origin');
+        this.pan.flattenOffset();
+        Animated.spring(this.pan, {
+          toValue: {x: 0, y: 0},
+          useNativeDriver: true,
+        }).start();
+        this.pan.setValue({x: 0, y: 0});
+        // console.log(this.pan);
+      }
+    },
+  });
+
   render() {
     const {quizData} = this.props;
     console.log('render drag&dropQuiz: props data:', quizData);
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          multiple choice: {quizData.questionContent}
-        </Text>
+      <View>
+        <Animated.View
+          style={{
+            transform: [{translateX: this.pan.x}, {translateY: this.pan.y}],
+          }}
+          {...this.panResponder.panHandlers}>
+          <View style={styles.box} />
+        </Animated.View>
       </View>
     );
   }
@@ -30,6 +76,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     paddingBottom: 15,
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'blue',
+    borderRadius: 5,
   },
 });
 

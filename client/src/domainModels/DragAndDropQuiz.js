4fa535478943eae4498d37f0010896b264e4e0e0
s3687636 <nguyenducminhtam83vn2003@gmail.com>
2020-04-09 10:28:13 +0700

    2nd stage: blue box fit to grey box, else go back to origial position

--- client/src/domainModels/DragAndDropQuiz.js
+++ client/src/domainModels/DragAndDropQuiz.js
@@ -2,11 +2,23 @@ import React, {Component} from 'react';
 import {Animated, View, StyleSheet, PanResponder, Text} from 'react-native';
 // import {goToFirstScreenInStack} from '../helpers/NavigateHelper';
 import PropTypes from 'prop-types';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+const initPosition = {x: 0, y: 100};
+const height = 150;
+const width = 150;
+const checkInRegtangle = (pan, wishPosition, height, width) => {
+  return (
+    pan.x._value > wishPosition.x - width &&
+    pan.x._value < wishPosition.x + width &&
+    pan.y._value > wishPosition.y - height &&
+    pan.y._value < wishPosition.y + height
+  );
+};
 
 class DragAndDropQuiz extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    // this.state = {};
     // this.onGestureEvent = Animated.event([
     //   {
     //     nativeEvent: {
@@ -17,9 +29,10 @@ class DragAndDropQuiz extends Component {
     //   },
     // ]);
   }
-  static propTypes = {
-    quizData: PropTypes.object.isRequired,
-  };
+  // static propTypes = {
+  //   quizData: PropTypes.object.isRequired,
+  // };
+
   pan = new Animated.ValueXY();
   panResponder = PanResponder.create({
     onMoveShouldSetPanResponder: () => true,
@@ -37,15 +50,23 @@ class DragAndDropQuiz extends Component {
       console.log('release at: ', this.pan);
       // alert('You dropped it at:' + this.pan.x._value + ' ' + this.pan.y._value);
       this.pan.flattenOffset();
-      if (this.pan.y._value < 0) {
+      // check if in the grey area, then fit to the grey
+      if (checkInRegtangle(this.pan, {x: 0, y: -height}, height, width)) {
         console.log('y < 0 : ', this.pan, 'moving back to origin');
-        this.pan.flattenOffset();
+        // this.pan.flattenOffset();
+        Animated.spring(this.pan, {
+          toValue: {x: 0, y: 0},
+          useNativeDriver: true,
+        }).start();
+        this.pan.setValue({x: -0.5, y: -151});
+        // console.log(this.pan);
+      } else {
+        // else go back to original position
         Animated.spring(this.pan, {
           toValue: {x: 0, y: 0},
           useNativeDriver: true,
         }).start();
         this.pan.setValue({x: 0, y: 0});
-        // console.log(this.pan);
       }
     },
   });
@@ -55,6 +76,8 @@ class DragAndDropQuiz extends Component {
     console.log('render drag&dropQuiz: props data:', quizData);
     return (
       <View>
+        {/* grey box: upper position compare to blue box */}
+        <View style={styles.wishBox} />
         <Animated.View
           style={{
             transform: [{translateX: this.pan.x}, {translateY: this.pan.y}],
@@ -70,17 +93,23 @@ class DragAndDropQuiz extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   text: {
     fontSize: 20,
     paddingBottom: 15,
   },
   box: {
-    height: 150,
-    width: 150,
+    height: height,
+    width: width,
     backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+  wishBox: {
+    height: height,
+    width: width,
+    backgroundColor: 'grey',
     borderRadius: 5,
   },
 });

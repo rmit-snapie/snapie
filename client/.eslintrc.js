68ef30fbc26b15274f26163b62cdb19fa4b81faa
trungduong0103 <trungduong0103@gmail.com>
2020-05-08 10:54:41 +0700

    switch to npx react-native instead of react-native cli, add react-native-track-player package and remove lodash

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        '@react-native-community'
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'indent': 'off',
+    'linebreak-style': [
+      'error',
+      'unix',
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
+    'quotes': [
+      'error',
+      'single',
     ],
-    'rules': {
-        'indent': 'off',
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-      'react/prop-types': ['error'],
-    }
+    'semi': [
+      'error',
+      'always',
+    ],
+    'react/prop-types': ['error'],
+  },
 };

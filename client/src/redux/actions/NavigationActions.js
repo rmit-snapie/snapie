7ab09bbd94b3fe169596a762c536de7d655e37d4
trungduong0103 <trungduong0103@gmail.com>
2020-05-16 15:14:10 +0700

    remove unused files

--- client/src/redux/actions/NavigationActions.js
+++
@@ -1,7 +0,0 @@
-import {SAVE_NAVIGATION} from '../types';
-// save current navigation stack to redux
-export const saveNavigationStack = navigationStack => {
-  return function(dispatch) {
-    dispatch({type: SAVE_NAVIGATION, payload: navigationStack});
-  };
-};
